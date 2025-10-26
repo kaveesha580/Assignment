87e613b2d1ee62e2af2c6b40a142a7535449b057
kaveesha580 <kaveeshadilshan580@gmail.com>
2025-10-26 10:32:32 +0530

    Main menu

--- assignment/src/assignment/Assignment.java
+++ assignment/src/assignment/Assignment.java
@@ -12,11 +12,52 @@ import java.io.FileWriter;
 import java.util.Scanner;
 public class Assignment {
 
-    /**
-     * @param args the command line arguments
-     */
+    static int MAX_CITIES = 30;
+    static double[][] allcost = new double[50][6];
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String[] cityname = new String[MAX_CITIES];
+        double[][] distance = new double[MAX_CITIES][MAX_CITIES];
+        int[][] vehicle = {{1000, 30, 60, 12}, {5000, 40, 50, 6}, {10000, 80, 45, 4}};
+        double [][] delivaryrequest = new double [50][9];
+        String[] vehicletype = {"Van", "Truck", "Lorry"};
+        
+        boolean result = true;
+        System.out.println("======================================================");
+        System.out.println("       Menu-driven logistics management system");
+        System.out.println("======================================================");
+        while (result) {
+            System.out.println("1.City Management");
+            System.out.println("2.Distance Management");
+            System.out.println("3.Vehicle Management ");
+            System.out.println("4.Delivery Request");
+            System.out.println("5.Delivery Records");
+            System.out.println("6.Performance Reports ");
+            System.out.println("7.Save & Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            System.out.println("---------------------------------------");
+            System.out.println("");
+
+            switch (choice) {
+                case 1 -> CityManagement(cityname);
+                case 2 -> DistanceManagement(distance, cityname);
+                case 3 -> VehicleManagement(vehicle, vehicletype);
+                case 4 -> Delivaryrequest(delivaryrequest, cityname, vehicle, distance, vehicletype);
+                case 5 -> DeliveryRecords(delivaryrequest, allcost, cityname, vehicletype);
+                case 6 -> reports(distance, delivaryrequest);
+                case 7 -> {
+                    saveRoutes(cityname, distance);
+                    saveDeliveries(delivaryrequest, allcost);
+                    System.out.println("Data saved successfully!");
+                    System.out.println("Exiting program...");
+                    result = false;
+                }
+                default -> System.out.println("Invalid number, try again...");
+            }
+        }
+    }
+
     }
     
-}
+
